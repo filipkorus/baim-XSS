c8ed7c3a36dcf974573d54c2256c29931896f4df
Filip Korus <filipkorus123@gmail.com>
2023-12-14 23:10:16 +0100

    bug resolved

--- backend/src/public/js/selectBackground.js
+++ backend/src/public/js/selectBackground.js
@@ -3,17 +3,28 @@ const selectBackground = (background) => {
 
     const buttonsContainer = document.querySelector("#select-background");
 
-    backgrounds.forEach(bg => {
+    const btn = document.createElement('button');
+    btn.innerHTML = background;
+    buttonsContainer.appendChild(btn);
+
+    for (let bg of backgrounds) {
+        if (bg === background) {
+            continue;
+        }
+
         const btn = document.createElement('button');
         btn.innerHTML = bg;
 
-        btn.addEventListener("click", () => {
-            // Update the value after # in the URL
-            window.location.hash = btn.innerHTML;
-            document.body.style.backgroundImage = `url('assets/${btn.innerHTML}-snowflake-background.jpg')`;
-        });
-
         buttonsContainer.appendChild(btn);
+    }
+
+    const buttons = buttonsContainer.querySelectorAll("button");
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            // Update the value after # in the URL
+            window.location.hash = button.innerHTML;
+            document.body.style.backgroundImage = `url('assets/${button.innerHTML}-snowflake-background.jpg')`;
+        });
     });
 
     const header = document.getElementsByClassName("header-container")[0];
