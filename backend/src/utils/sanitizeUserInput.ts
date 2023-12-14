17782e62f46d4b0d381bd07c82d092af00d25167
Filip Korus <filipkorus123@gmail.com>
2023-12-14 02:35:24 +0100

    finished

--- backend/src/utils/sanitizeUserInput.ts
+++ backend/src/utils/sanitizeUserInput.ts
@@ -1,12 +1,8 @@
 import sanitizeHtml from 'sanitize-html';
 
 const sanitizeUserInput = (input: string): string => {
-	const sanitizedInput = sanitizeHtml(input, {
-		allowedTags: ['b', 'i', 'em', 'strong', 'a'],
-		allowedAttributes: {
-			'a': ['href'] // allow attribute 'href' in 'a' tag
-		}
-	});
+	const sanitizedInput = input;
+
 	return sanitizedInput;
 }
 
